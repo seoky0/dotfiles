84088d57a951a722c45d8b72c4e3c67c42f8cbc1
kyohei0423 <kyo199333@gmail.com>
2018-05-10 12:22:43 +0900

    Homebrewをインストール

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 if [ "$(uname)" ==  "Darwin" ]; then
-    echo "Set up Mac"
+  echo "Set up Mac"
+  # todo
+  # brew, git, anyenv, rbenv, ndenv, pyenv, fish, tmuxのインストール
+  # rbenvでrubyをインストール
+  # ndenvでnodeをインストール
+  # yarnをインストール
+  if type "brew" > /dev/null 2>&1; then
+    echo "Homerew is installed"
+  else
+    echo "Homebrew is installing"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 elif [ "$(uname)" == "Linux" ]; then
-    echo "Set up Linux"
+  echo "Set up Linux"
 fi
