8c01f4fbcc9a8cbd15381936a9a2291ba5daf8f6
瀬尾 匡平 <Kyohei@seo-no-MacBook-Pro.local>
2018-06-05 18:27:58 +0900

    anyenvで管理する

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,54 @@
 #!/bin/sh
 
 if [ "$(uname)" ==  "Darwin" ]; then
-    echo "Set up Mac"
+    echo "Set up for Mac"
+    echo "Set up font"
+    git clone https://github.com/powerline/fonts.git --depth=1
+    cd fonts
+    ./install.sh
+    cd ..
+    rm -rf fonts
+
+    echo "Set up shell"
+    brew install fish
+    curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+    fisher omf/theme-agnoster
+
+    echo "Set up anyenv and envs"
+    git clone https://github.com/riywo/anyenv ~/.anyenv
+    echo 'set --export PATH  "$HOME/.anyenv/bin" $PATH' >> ~/.config/fish/config.fish
+    exec fish -l
+    mkdir $HOME/.anyenv/envs
+    anyenv pyenv
+    anyenv ndenv
+    anyenv rbenv
+    anyenv goenv
+    exec fish -l
+
+    echo "Install programming languages"
+    pyenv install 3.6.5
+    pyenv install 2.7.15
+    pyenv global 2.7.15
+    pyenv rehash
+    rbenv install 2.5.1
+    rbenv global 2.5.1
+    rbenv rehash
+    ndenv install v10.3.0
+    ndenv global v10.3.0
+    ndenv rehash
+    goenv install 1.10.2
+    goenv global 1.10.2
+    goenv rehash
+    exec fish -l
+
+    echo "Set up neovim"
+    brew install neovim
+    gem install neovim
+    pip2 install neovim
+    pyenv global 3.6.5
+    pyenv rehash
+    pip3 install neovim
+
 elif [ "$(uname)" == "Linux" ]; then
-    echo "Set up Linux"
+    echo "Set up for Linux"
 fi
