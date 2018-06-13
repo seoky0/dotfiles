3a3a0dd6d53f17e995387dda94b9ec1c706b0f5e
kyohei0423 <kyo199333@gmail.com>
2018-06-13 23:01:38 +0900

    コメントアウト

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -73,6 +73,6 @@ alias tmux-ks="tmux kill_server"
 function fish_user_key_bindings
   #peco
   bind \cr peco_select_history
-  bind \cf peco_change_directory
+  # bind \cf peco_change_directory
 end
 
