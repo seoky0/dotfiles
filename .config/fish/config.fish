b7a1a5be402845c9831ee803a07fda10f8866f75
kyohei0423 <kyo199333@gmail.com>
2018-06-13 21:27:11 +0900

    tmuxのaliasを追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -63,6 +63,12 @@ alias grmtp="git push -u origin"          # リモートリポジトリにプッ
 alias gpul="git pull origin"
 alias gpus="git push origin"
 
+# tmuxエイリアス
+alias tmux-n="tmux new -s"
+alias tmux-a="tmux a -t"
+alias tmux-k="tmux kill-session -t"
+alias tmux-ks="tmux kill_server"
+
 # keybind
 function fish_user_key_bindings
   #peco
