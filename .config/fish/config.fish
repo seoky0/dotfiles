113b46fce8fa54946f140777ccd87561887f9ab2
kyohei0423 <kyo199333@gmail.com>
2018-06-15 09:09:09 +0900

    ディレクトリ検索（動いていない）を追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -77,6 +77,7 @@ alias tmux-ks="tmux kill_server"
 function fish_user_key_bindings
   #peco
   bind \cr peco_select_history
+  # あとでpeco_change_directory.fishを修正する  
   # bind \cf peco_change_directory
 end
 
